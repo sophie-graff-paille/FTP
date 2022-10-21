f2c07fa686e85f15fad4c791682532ddcea4d445
sophie-graff-paille <sophie.graff-paille@laplateforme.io>
2022-10-21 20:27:55 +0200

    Projet FTP

--- Job 8/purge-FTP.sh
+++ Job 8/purge-FTP.sh
@@ -5,3 +5,6 @@ sudo userdel -r Merry && sudo userdel -r Pippin
 
 # Désinstalle proftpd et sa configuration
 sudo apt-get autoremove --purge proftpd
+
+# Désinstalle openssl et sa configuration
+sudo apt-get remove --purge openssl
